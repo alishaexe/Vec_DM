1840ff1b0aad08626c7dc8fe44b816e38d512eba
alishaexe <119526666+alishaexe@users.noreply.github.com>
2024-12-02 13:56:03 +0000

    overlays

--- JaxIntegration.py
+++ JaxIntegration.py
@@ -226,10 +226,11 @@ print(mins, "Minutes")
 
 #Last time with 7000 in integrations and 200 x vals it took 8 mins
 
-
+te = np.array(hope)
 #%%
 
 plt.loglog(xt, hope)
+plt.loglog(x3, y3)
 # plt.axline((x1,y1),(x2,y2), color = 'r')
 plt.xlabel(r"$x_\star$", fontsize = 16)
 # plt.ylabel(r"$\frac{M^4_{Pl}}{H^4_I}\Omega_{GW}$")
@@ -245,7 +246,203 @@ plt.show()
 #%%
 
 def func(x):
-    return 1e-5*x**2.5
+    return 1e-4*(x)**2.5
 
-x1, x2 = 0.15, 0.7
+x1, x2 = 0.2, 0.5
+x3 = jnp.linspace(0.2, 0.8, 10)
 y1, y2 = func(x1), func(x2)
+y3 = func(x3)
+# y1, y2 = 1e-7, 1.2e-4
+
+plt.loglog(xt, te)
+plt.loglog(x3, y3)
+# plt.axline((x1,y1),(x2,y2), color = 'r')
+plt.xlabel(r"$x_\star$", fontsize = 16)
+# plt.ylabel(r"$\frac{M^4_{Pl}}{H^4_I}\Omega_{GW}$")
+plt.ylabel(r"$\Omega_{GW}$")
+plt.axhline(y=5e-6, color='r', linestyle='--', label="y = 0.5")
+# plt.title("method 1")
+# # plt.ylim(1e-5, 3e-4)
+plt.grid(True)
+# plt.savefig('/Users/alisha/Documents/Vec_DM/JaxOmegGW.png', bbox_inches='tight')
+plt.show()
+
+#%%
+from scipy.interpolate import UnivariateSpline
+#%%
+ym = np.log10(te)
+xm = np.log10(xt)
+spline = UnivariateSpline(xm, ym, s=0.5)
+
+ysmoo = 10**spline(xm)
+
+plt.figure(figsize=(8, 5))
+# plt.loglog(xt, te, label='Original Data', alpha=0.7)
+plt.loglog(xt, ysmoo, label='Smoothed Curve', linewidth=2)
+plt.legend()
+plt.show()
+#%%
+plt.loglog(xt, ysmoo, label = "Python")
+plt.loglog(xdat, ydat, '--', markersize=5, label='Data')
+plt.legend()
+plt.grid(True)
+
+#%%
+data = [
+    [0.2, 1.68575e-6], [0.218547, 2.09599e-6], [0.238813, 2.60489e-6], 
+    [0.260959, 3.28666e-6], [0.285159, 4.17816e-6], [0.311603, 5.20304e-6],
+    [0.340499, 6.28363e-6], [0.372075, 7.53625e-6], [0.406578, 9.18055e-6], 
+    [0.444282, 1.12799e-5], [0.485482, 1.35966e-5], [0.530502, 1.5915e-5], 
+    [0.579697, 1.89958e-5], [0.633455, 2.30602e-5], [0.692197, 2.5934e-5], 
+    [0.756387, 2.94267e-5], [0.82653, 3.50427e-5], [0.903177, 3.80892e-5], 
+    [0.986931, 4.40926e-5], [1.07845, 4.77267e-5], [1.17846, 5.48365e-5],
+    [1.28774, 5.84677e-5], [1.40716, 6.52861e-5], [1.53765, 7.09824e-5], 
+    [1.68025, 7.60901e-5], [1.83606, 8.20633e-5], [2.00632, 8.83102e-5], 
+    [2.19238, 9.38795e-5], [2.39569, 9.80632e-5], [2.61785, 1.01594e-4], 
+    [2.86061, 1.04401e-4], [3.12588, 1.06852e-4], [3.41576, 1.08298e-4], 
+    [3.73251, 1.09993e-4], [4.07864, 1.10491e-4], [4.45687, 1.11707e-4], 
+    [4.87017, 1.12119e-4], [5.3218, 1.12159e-4], [5.8153, 1.12411e-4], 
+    [6.35458, 1.12717e-4], [6.94386, 1.12775e-4], [7.58779, 1.12734e-4], 
+    [8.29143, 1.12672e-4], [9.06033, 1.12485e-4], [9.90052, 1.12151e-4], 
+    [10.8186, 1.12031e-4], [11.8219, 1.11643e-4], [12.9182, 1.11308e-4], 
+    [14.1161, 1.10919e-4], [15.4252, 1.10489e-4], [16.8556, 1.09984e-4], 
+    [18.4187, 1.09314e-4], [20.1267, 1.0859e-4], [21.9931, 1.07804e-4], 
+    [24.0326, 1.06799e-4], [26.2612, 1.05714e-4], [28.6965, 1.044e-4], 
+    [31.3577, 1.02936e-4], [34.2656, 1.0123e-4], [37.4431, 9.93014e-5], 
+    [40.9154, 9.70536e-5], [44.7096, 9.45595e-5], [48.8557, 9.17463e-5], 
+    [53.3862, 8.86179e-5], [58.3369, 8.50337e-5], [63.7467, 8.11187e-5], 
+    [69.6582, 7.67686e-5], [76.1178, 7.21193e-5], [83.1765, 6.70595e-5], 
+    [90.8898, 6.16338e-5], [99.3183, 5.59066e-5], [108.528, 4.99202e-5], 
+    [118.593, 4.38206e-5], [129.59, 3.77656e-5], [141.608, 3.1894e-5], 
+    [154.739, 2.63081e-5], [169.089, 2.11025e-5], [184.769, 1.64908e-5], 
+    [201.903, 1.26811e-5], [220.627, 9.85718e-6], [241.086, 7.9531e-6], 
+    [263.443, 6.73748e-6], [287.873, 6.01661e-6], [314.568, 5.66114e-6], 
+    [343.739, 5.57368e-6], [375.615, 5.44249e-6], [410.448, 4.87802e-6], 
+    [448.51, 4.03016e-6], [490.102, 3.27005e-6], [535.551, 2.64619e-6], 
+    [585.214, 2.11117e-6], [639.483, 1.8003e-6], [698.785, 1.83914e-6], 
+    [763.586, 1.59903e-6], [834.396, 1.36485e-6], [911.772, 1.24355e-6], 
+    [996.324, 9.94898e-7], [1088.72, 8.61792e-7], [1189.68, 8.04384e-7], 
+    [1300.0, 7.45641e-7]
+]
+
+# Split data into x and y vectors
+xdat, ydat = np.array(data).T
+#%%
+
+fgw = 7.5e-6
+def fOmeg(y):
+    res = y*1e-6
+    return res
+
+def freq(f):
+    res = f*fgw
+    return res
+
+omegdat = np.array(list(map(fOmeg, ydat)))
+fdat = np.array(list(map(freq, xdat)))
+
+plt.loglog(fdat, omegdat)
+plt.show()
+
+omegme = np.array(list(map(fOmeg, ysmoo)))
+fme = np.array(list(map(freq, xt)))
+
+plt.loglog(fme, omegme)
+plt.loglog(fdat, omegdat)
+plt.show
+
+#%%
+yr = 365*24*60*60 #in seconds
+H0 = 100*0.67*10**(3)/(3.086*10**(22)) #1/seconds
+# H0 = 3.24e-18 #Debikas value
+#setting h = 0.67
+pi = np.pi
+c = 3e8
+fetstar = 10**(-2)
+fi = 0.4*10**(-3)
+
+#For the LISA mission they have designed the
+#arms to be length L = 2.5*10^(6)
+T = 3*yr
+snr5 = 5
+#L = 2.5e9
+L = 25/3
+
+c = 3e8
+fetstar = 10**(-2)
+fi = 0.4*10**(-3)
+
+fLisa = 1/(2*pi*L)
+ffmin = 10**(-5)
+ffmax = 445
+elmin = np.log10(ffmin)
+elmax = np.log10(ffmax)
+###############
+#Change this value for how many 'steps' you want in the range of values
+
+itera = 100
+nitera = 10
+##########
+
+elminL = (np.log10(ffmin))
+elmaxL = (np.log10(10**(-0.95)))
+elminet = np.log10(1.6)
+elmaxet = (np.log10(ffmax))
+ntmin = -9/2
+ntmax = 9/2
+step = (ntmax-ntmin)/itera
+
+
+P = 12
+A = 3
+
+
+def P_acc(f):
+    res = A**2 *(1e-15)**2 * (1+(0.4e-3 / f)**2)*(1+(f/8e-3)**4)*(2*pi*f)**(-4)*(2*pi*f/c)**2
+    return res
+
+def P_ims(f):#* (1e-12)**2 after P
+    res = P**2 * (1e-12)**2 *(1+(2e-3/f)**4)*(2*pi*f/c)**2
+    return res
+
+def N_aa(f):
+    con = 2*pi*f*L
+    res = 8 * (np.sin(con))**2 * (4*(1+np.cos(con)+(np.cos(con))**2)*P_acc(f)+(2+np.cos(con))*P_ims(f))
+    return res
+
+def R(f):
+    res = 16*(np.sin(2*pi*f*L))**2  * (2*pi*f*L)**2 * 9/20 * 1/(1+0.7*(2*pi*f*L)**2)
+    return res
+
+def S_n(f):
+    res = N_aa(f)/R(f)
+    return res
+
+def Ohms(f):
+    const = 4*pi**2/(3*H0**2)
+    res = const *f**3*S_n(f)
+    return res
+
+freqvals = np.logspace(elminL, elmaxL, 200)   
+sigvals = np.array(list(map(Ohms, freqvals)))
+
+
+#%%
+fbplo = np.load("ftablisa.npy")
+
+plt.figure(figsize=(6, 7))
+plt.loglog(freqvals, sigvals, label = "Nominal Curve", color = "indigo", linewidth=1.5)
+plt.loglog(np.exp(fbplo[:,0]), np.exp(fbplo[:,1]), label = "BPLS curve", color = "lime", linewidth=1.5)
+plt.loglog(fme, omegme, label="PythonCurve")
+plt.loglog(fdat, omegdat, label = "DataCurve")
+plt.ylabel(r"$\Omega_{GW}$")
+plt.xlabel(r"$f (Hz)$")
+plt.legend()
+plt.grid(True)
+plt.savefig('/Users/alisha/Documents/Vec_DM/OverlayOmegGW.png', bbox_inches='tight')
+plt.show()
+
+#%%
+
+
+
